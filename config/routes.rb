cefad3f6592d6039335bc9a4153a6b7dc746b8fe
Kirstan Fitzptrick <you@example.com>
2017-06-07 02:42:44 +0000

    rails generate scaffold menu_item name:string description:text price:decimal

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :menu_items
   devise_for :users
 	root "static_pages#index"
 end
---
+++ spec/views/menu_items/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "menu_items/index", type: :view do
+  before(:each) do
+    assign(:menu_items, [
+      MenuItem.create!(
+        :name => "Name",
+        :description => "MyText",
+        :price => "9.99"
+      ),
+      MenuItem.create!(
+        :name => "Name",
+        :description => "MyText",
+        :price => "9.99"
+      )
+    ])
+  end
+
+  it "renders a list of menu_items" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "tr>td", :text => "9.99".to_s, :count => 2
+  end
+end
