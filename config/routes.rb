f0734ded0de4ce3aac3b1f31dd40fc49b221f996
Kirstan Fitzptrick <you@example.com>
2017-07-06 15:11:59 +0000

    Commit to push new branch to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 	devise_for :users
-	root :to => 'static_pages#index'
+	root 'static_pages#index'
 	resources :cart_items, only: [:create]
 	resources :carts
 	resources :foods, only: [:index, :show]
 	resources :business_hours, only: [:index, :show]
-  resources :menu_items, only: [:index, :show]
+  resources :menu_items, only: [:index, :show] do
   	resources :images, only: [:create, :show]
+  end
 	namespace :administrator do
 		resources :menu_items
 		resources :business_hours
