72d53bfacb34f0e897626414fbb0d66a83e27983
Kirstan Fitzptrick <you@example.com>
2017-07-10 19:33:11 +0000

    Adjusted styling in app/views/menu_items, and app/views/business_hours, and app/views/static_pages

--- app/controllers/administrator/menu_items_controller.rb
+++ app/controllers/administrator/menu_items_controller.rb
@@ -2,7 +2,7 @@ class Administrator::MenuItemsController < ApplicationController
 	before_action :authenticate_user!
 	
 	def index
-		
+		@menu_items = MenuItem.all
 	end
 
 	def new
@@ -31,7 +31,7 @@ class Administrator::MenuItemsController < ApplicationController
   def destroy
     @menu_item = MenuItem.find(params[:id])
     @menu_item.destroy
-    redirect_to menu_items_path
+    redirect_to menu_item_path
   end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 	devise_for :users
 	root 'static_pages#index'
-	resources :cart_items, only: [:create]
+	resources :cart_items, only: :create
 	resources :carts
 	resources :foods, only: [:index, :show]
 	resources :business_hours, only: [:index, :show]
   resources :menu_items, only: [:index, :show] 
-  	resources :images, only: [:create, :show]
-  
+  resources :images, only: [ :create, :show]
+  	
 	namespace :administrator do
-		resources :menu_items
+		resources :menu_items 
 		resources :business_hours
 		resources :food_types
 		resources :foods
